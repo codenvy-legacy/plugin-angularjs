79d490adc5ed04b9569ca0bb2712a6b2e23e16b0
Ann Shumilova <ashumilova@codenvy.com>
2014-04-24 14:53:31 +0300

    IDEX-582: replace file icons.

--- core/client/src/main/java/com/codenvy/plugin/angularjs/core/client/editor/AngularJSResources.java
+++ core/client/src/main/java/com/codenvy/plugin/angularjs/core/client/editor/AngularJSResources.java
@@ -20,6 +20,8 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
 
+import org.vectomatic.dom.svg.ui.SVGResource;
+
 /**
  * Defines image used for tab completion for AngularJS.
  *
@@ -33,4 +35,43 @@ public interface AngularJSResources extends ClientBundle {
     @Source("com/codenvy/plugin/angularjs/core/client/completion-item-angularjs.png")
     ImageResource property();
 
+    
+    @Source("com/codenvy/plugin/angularjs/core/client/svg/bower.svg")
+    SVGResource bowerFile();
+    
+    @Source("com/codenvy/plugin/angularjs/core/client/svg/css.svg")
+    SVGResource cssFile();
+    
+    @Source("com/codenvy/plugin/angularjs/core/client/svg/grunt.svg")
+    SVGResource gruntFile();
+    
+    @Source("com/codenvy/plugin/angularjs/core/client/svg/gulp.svg")
+    SVGResource gulpFile();
+    
+    @Source("com/codenvy/plugin/angularjs/core/client/svg/html.svg")
+    SVGResource htmlFile();
+    
+    @Source("com/codenvy/plugin/angularjs/core/client/svg/image-icon.svg")
+    SVGResource imageIcon();
+    
+    @Source("com/codenvy/plugin/angularjs/core/client/svg/json.svg")
+    SVGResource jsonFile();
+    
+    @Source("com/codenvy/plugin/angularjs/core/client/svg/js.svg")
+    SVGResource jsFile();
+    
+    @Source("com/codenvy/plugin/angularjs/core/client/svg/less.svg")
+    SVGResource lessFile();
+    
+    @Source("com/codenvy/plugin/angularjs/core/client/svg/maven.svg")
+    SVGResource mavenFile();
+    
+    @Source("com/codenvy/plugin/angularjs/core/client/svg/npm.svg")
+    SVGResource npmFile();
+    
+    @Source("com/codenvy/plugin/angularjs/core/client/svg/text.svg")
+    SVGResource textFile();
+    
+    @Source("com/codenvy/plugin/angularjs/core/client/svg/xml.svg")
+    SVGResource xmlFile();
 }
