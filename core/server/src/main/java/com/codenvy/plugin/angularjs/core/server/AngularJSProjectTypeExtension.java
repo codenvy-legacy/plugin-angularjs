4d6c57af21d4831c7b1833a14d1fe6566e316928
Florent Benoit <Florent.Benoit@serli.com>
2014-08-07 14:09:26 +0200

    Change constant package

--- core/server/src/main/java/com/codenvy/plugin/angularjs/core/server/AngularJSProjectTypeExtension.java
+++ core/server/src/main/java/com/codenvy/plugin/angularjs/core/server/AngularJSProjectTypeExtension.java
@@ -15,7 +15,7 @@ import com.codenvy.api.project.server.ProjectTypeExtension;
 import com.codenvy.api.project.shared.Attribute;
 import com.codenvy.api.project.shared.ProjectTemplateDescription;
 import com.codenvy.api.project.shared.ProjectType;
-import com.codenvy.ide.ext.java.shared.Constants;
+import com.codenvy.ide.Constants;
 import com.codenvy.plugin.angularjs.api.server.AngularProjectTemplateExtension;
 
 import javax.annotation.PostConstruct;
